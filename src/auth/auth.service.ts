a8ef46ebf4c8d553f5176862c3479dd6335c5494
key-68 <tkhoa06082005@gmailcom>
2025-09-26 15:57:22 +0700

    set up passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '@/modules/users/users.service';
 import { comparePasswordHelper } from '@/helpers/util';
@@ -11,16 +10,18 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!isValidPassword) {
-      throw new UnauthorizedException("username/password is invalid!");
-    } 
-    
-    const payload = { sub: user._id, username: user.email}
+    if (!user || !isValidPassword) return null;
+
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
     return {
-      access_token: await this.jwtService.signAsync(payload)
-    }
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
